3d80ee29c921c6b4ffd1da03d54a68c9d5fddcad
Maddie_Saxby <saxby@unlv.nevada.edu>
2026-09-21 18:48:15 -0700

    Fix formatting issue in testtracing.c

--- user/testtracing.c
+++ user/testtracing.c
@@ -1,1 +1,20 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
 
+int main(void){
+  int ticks = uptime();
+  printk("Uptime before trace: %d\n", ticks);
+  printk("Enabling tracing...\n");
+  syscalltrace(1);
+
+  getpid();
+  uptime();
+  sleep(1);
+
+  int pid = fork();
+
+  if (pid < 0){
+  printk("Fork failed\n");
+  exit(1);
+  }
