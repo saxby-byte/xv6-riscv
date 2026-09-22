988ca09fe3e91d2a3ca5336ddab53349e0779813
Maddie_Saxby <saxby@unlv.nevada.edu>
2026-09-21 18:17:03 -0700

    Disable tracing by default for new processes

--- kernel/proc.c
+++ kernel/proc.c
@@ -124,7 +124,7 @@ allocproc(void)
 found:
   p->pid = allocpid();
   p->state = USED;
-
+  p->trace_enabled = 0; //ensure tracing is disabled by default
   // Allocate a trapframe page.
   if ((p->trapframe = (struct trapframe *)kalloc()) == 0) {
     freeproc(p);
