b56c12a82da23295d41a970a7a6cae5c591fc3d0
Maddie_Saxby <saxby@unlv.nevada.edu>
2026-09-21 18:14:47 -0700

    Add trace_enabled field to process structure

--- kernel/proc.h
+++ kernel/proc.h
@@ -88,7 +88,7 @@ struct proc {
   int killed;           // If non-zero, have been killed
   int xstate;           // Exit status to be returned to parent's wait
   int pid;              // Process ID
-
+  int trace_enabled;    // if tracing is disabled its 0 if enabled its 1
   // wait_lock must be held when using this:
   struct proc *parent; // Parent process
 
