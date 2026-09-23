ba726eb969152d57c0d88f28c21eafa87be366fe
Maddie_Saxby <saxby@unlv.nevada.edu>
2026-09-23 14:47:45 -0700

    Refactor syscall handling with tracing support

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -169,12 +169,12 @@ syscall(void)
 
   num = p->trapframe->a7;
   if (num > 0 && num < NELEM(syscalls) && syscalls[num]) {
-     // Use num to lookup the system call function for num, call it,
-    // and store its return value in p->trapframe->a0
-    p->trapframe->a0 = syscalls[num]();
     if (p->trace_enabled){
       printk("Syscall Name: %s\nProcess PID: %d\n", syscall_names[num], p->pid);
     }
+     // Use num to lookup the system call function for num, call it,
+    // and store its return value in p->trapframe->a0
+    p->trapframe->a0 = syscalls[num]();
   } else {
     printk("%d %s: unknown sys call %d\n", p->pid, p->name, num);
     p->trapframe->a0 = -1;
