9ff1e58ec2f69c3c0602813f6a4d33dc86948398
Maddie_Saxby <saxby@unlv.nevada.edu>
2026-09-23 14:28:41 -0700

    Refactor tracing output and syscalltrace usage

--- user/testtracing.c
+++ user/testtracing.c
@@ -4,13 +4,14 @@
 
 int main(int argc, char *argv[]){
   int ticks = uptime();
-  printf("Process Uptime: %d\n", ticks);
-  printf("Enabling tracing...\n");
+  printf("Process Uptime: %d\n", uptime());
+  printf("Testing syscalltrace() implementation...\n");
   syscalltrace(1);
-
+  sbrk(0);
   getpid();
   uptime();
-
+  sleep(1);
+  
   int pid = fork();
 
   if (pid < 0){
@@ -18,13 +19,12 @@ int main(int argc, char *argv[]){
   exit(1);
   }
   if (pid == 0){
-  printf("Child process checking syscalltrace status...\n");
-  getpid();
+  printf("Child Tracking Value: %d\n", 0);
+  uptime();
   exit(0);
   }else{
-  wait(0);
-  syscalltrace(0);
-  printf("Tracing disabled. \n");
+  printf("Parent Tracking Value: %d\n", 1);
+    wait(0);
   }
   exit(0);
 }
