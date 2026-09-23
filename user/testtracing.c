397ec7d381a333d67078c00008aef9e4490b1474
Maddie_Saxby <saxby@unlv.nevada.edu>
2026-09-23 15:22:57 -0700

    Update syscalltrace call with an argument

--- user/testtracing.c
+++ user/testtracing.c
@@ -6,7 +6,7 @@ int main(void){
   int ticks = uptime();
   printf("Process Uptime: %d\n", ticks);
   printf("Testing syscalltrace() implementation...\n");
-  syscalltrace();
+  syscalltrace(1);
   sbrk(0);
   getpid();
   pause(1);
