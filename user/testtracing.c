c629a369d644207f2595e5743cb5c0aba60906d3
Maddie_Saxby <saxby@unlv.nevada.edu>
2026-09-23 14:18:02 -0700

    Replace printk with printf for tracing messages

--- user/testtracing.c
+++ user/testtracing.c
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[]){
   int ticks = uptime();
   printf("Uptime before trace: %d\n", ticks);
-  printk("Enabling tracing...\n");
+  printf("Enabling tracing...\n");
   syscalltrace(1);
 
   getpid();
@@ -15,17 +15,17 @@ int main(int argc, char *argv[]){
   int pid = fork();
 
   if (pid < 0){
-  printk("Fork failed\n");
+  printf("Fork failed\n");
   exit(1);
   }
   if (pid == 0){
-  printk("Child process checking syscalltrace status...\n");
+  printf("Child process checking syscalltrace status...\n");
   getpid();
   exit(0);
   }else{
   wait(0);
   syscalltrace(0);
-  printk("Tracing disabled. \n");
+  printf("Tracing disabled. \n");
   }
   exit(0);
 }
